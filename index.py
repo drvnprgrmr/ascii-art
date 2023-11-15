1313a61e8e9e1785213d5c67f4b540d2d1b541a3
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2023-11-16 00:14:39 +0100

    section 2 complete

--- index.py
+++ index.py
@@ -2,7 +2,14 @@ from PIL import Image
 
 img = Image.open("./images/ascii-pineapple.jpg")
 
-img_width, img_height = img.size
+width, height = img.size
 
-print("Image loaded successfully!")
-print(f"Image size: {img_width} x {img_height}")
+pixels = list(img.getdata())
+pixel_grid = [ pixels[i * width: (i + 1) * width] for i in range(height)]
+
+print(len(pixels))
+if __name__ == "__main__":
+    print("Image loaded successfully!")
+    print(f"Image size: {width} x {height}")
+    for row in pixel_grid:
+        print(row)
