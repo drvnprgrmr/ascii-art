e048bd3e08e6c5afcb19dc2b75702bb87ed6daec
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2023-11-16 00:59:34 +0100

    all sections complete

--- index.py
+++ index.py
@@ -1,15 +1,35 @@
 from PIL import Image
+from time import sleep
 
 img = Image.open("./images/ascii-pineapple.jpg")
 
+img = img.resize((200, 200))
+
 width, height = img.size
 
+print("Image loaded successfully!")
+print(f"Image size: {width} x {height}")
+
+sleep(.5)
+
+# Get pixels of image
 pixels = list(img.getdata())
 pixel_grid = [ pixels[i * width: (i + 1) * width] for i in range(height)]
 
-print(len(pixels))
-if __name__ == "__main__":
-    print("Image loaded successfully!")
-    print(f"Image size: {width} x {height}")
-    for row in pixel_grid:
-        print(row)
+brightness_matrix = [[sum(pixel) / 3 for pixel in row] for row in pixel_grid]
+
+ASCII_BRIGHTNESS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+# Convert brightness matrix to ascii matrix
+ascii_matrix = []
+for row in brightness_matrix:
+    ascii_row = []
+    for brightness in row:
+        ascii_idx = int(brightness / 255 * len(ASCII_BRIGHTNESS))
+        ascii_char = ASCII_BRIGHTNESS[ascii_idx]
+        ascii_row.append(ascii_char * 3)
+    ascii_matrix.append(ascii_row)
+
+for row in ascii_matrix:
+    print("".join(row))
+    sleep(.05)
